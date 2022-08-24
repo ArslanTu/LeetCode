c2fa9109b676d1d907387e41579de021a1499f54
ArslanTu <kazamashiron@outlook.com>
2022-08-24 17:42:14 +0800

    complete 21.合并两个有序链表

---
+++ 21.合并两个有序链表.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/*
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+*/
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==nullptr) return list2;
+        if(list2==nullptr) return list1;
+
+        ListNode list3_head;
+        ListNode* p_list3_head=&list3_head; //最后应该返回p_list3_head.next()
+        ListNode* p = p_list3_head;
+
+        while (list1!=nullptr&&list2!=nullptr)
+        {
+            if(list1->val<list2->val){
+                p->next=list1;
+                list1=list1->next;
+                p=p->next;
+            }
+            else{
+                p->next=list2;
+                list2=list2->next;
+                p=p->next;
+            }
+        }
+        if(list1!=nullptr&&list2!=nullptr) return p_list3_head->next;
+        ListNode* temp_tail = list1==nullptr?list2:list1;
+        p->next=temp_tail;
+        return p_list3_head->next;
+    }
+};
+// @lc code=end
+
