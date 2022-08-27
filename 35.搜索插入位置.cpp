21d2c2cfa37b9094d752eaa662d90c1046e91ca7
ArslanTu <kazamashiron@outlook.com>
2022-08-27 15:52:20 +0800

    complete 35.搜索插入位置

---
+++ 35.搜索插入位置.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=cpp
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+//考察二分法
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1,mid=(low+high)>>1;
+        for(;low<high;mid=(low+high)>>1){
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<target) low=mid+1;
+            if(nums[mid]>target) high=mid-1;
+        }
+        if(low==high) return nums[low]<target?low+1:low;
+        //不会发生low=mid+1后low>high
+        //只有high=mid-1后low>high
+        //此时high位元素小于目标，low位元素大于目标
+        return low;
+    }
+};
+// @lc code=end
+
