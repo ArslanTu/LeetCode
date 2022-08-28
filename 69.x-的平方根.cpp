857a87e3e6ca23eb1547f7908b037eac5b593834
ArslanTu <kazamashiron@outlook.com>
2022-08-28 17:26:34 +0800

    complete 69.x 的平方根

---
+++ 69.x-的平方根.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=cpp
+ *
+ * [69] x 的平方根 
+ */
+
+// @lc code=start
+/*
+使用牛顿迭代法
+*/
+
+#include <math.h>
+
+using std::abs;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2) return x;
+        double root=x>>1;
+        while(abs(root*root - x)>=1) root=(root+x/root)/2;
+        return int(root);
+    }
+};
+// @lc code=end
+
