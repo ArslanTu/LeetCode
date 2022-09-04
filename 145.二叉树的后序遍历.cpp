be29d14358201a9833590f008f7c082cf5106dec
ArslanTu <kazamashiron@outlook.com>
2022-09-04 13:15:18 +0800

    complete problem No.145

---
+++ 145.二叉树的后序遍历.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=cpp
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+
+using std::vector;
+/*
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+*/
+
+class Solution {
+public:
+    vector<int> res;
+    vector<int> postorderTraversal(TreeNode* root) {
+        afs(root);
+        return res;
+    }
+    void afs(TreeNode* root){
+        if(root==nullptr) return;
+        afs(root->left);
+        afs(root->right);
+        res.insert(res.end(),root->val);
+    }
+};
+// @lc code=end
+
