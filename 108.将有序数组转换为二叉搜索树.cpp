807fb778670c81a95aa1eadd780fa23572cdef1f
ArslanTu <kazamashiron@outlook.com>
2022-09-06 17:10:32 +0800

    unfished No.108&168

---
+++ 108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+
+using std::vector;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int low=0,high=nums.size()-1;
+        if(low<=high){
+            TreeNode* root = new TreeNode(nums[nums.size()>>1]);
+            vector<int> numsL(nums,nums+1);
+            vector<int> numsR();
+            root->left=sortedArrayToBST(numsL);
+            root->right=sortedArrayToBST(numsR);
+            return root;
+        }
+        else return nullptr;
+    }
+};
+// @lc code=end
+
---
+++ 168.excel表列名称.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=168 lang=cpp
+ *
+ * [168] Excel表列名称
+ */
+
+// @lc code=start
+#include <string>
+using std::string;
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string s="";
+        while(columnNumber!=0){
+        }
+        string res(s.rbegin(),s.rend());
+        return res;
+    }
+};
+// @lc code=end
+
