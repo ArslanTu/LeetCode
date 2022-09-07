a82a6130605220a50d5b6aa713f8550f0c6075cb
ArslanTu <kazamashiron@outlook.com>
2022-09-07 20:05:56 +0800

    complete problem No.168

--- 168.excel表列名称.cpp
+++ 168.excel表列名称.cpp
@@ -12,6 +12,9 @@ public:
     string convertToTitle(int columnNumber) {
         string s="";
         while(columnNumber!=0){
+            int num = columnNumber%26==0?26:columnNumber%26;
+            s+=char(num+64);
+            columnNumber = (columnNumber-num)/26;
         }
         string res(s.rbegin(),s.rend());
         return res;
